38be0ce1500efa4d8239d669dc1411a3d705f8d5
footzi <footzi@mail.ru>
2019-05-19 23:33:10 +0300

    Добавляет redux в native

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import Home from "./mobile/components/Home";
 import { StyleSheet, Text, View } from "react-native";
+import store from "./store";
+import { Provider } from "react-redux";
+
+console.log(store);
 
 export default class App extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Hello!</Text>
-            </View>
+            <Provider store={store()}>
+                <View style={styles.container}>
+                    <Text>Hello!</Text>
+                    <Home />
+                </View>
+            </Provider>
         );
     }
 }
--- mobile/components/Home.js
+++ mobile/components/Home.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
+import { Button } from "react-native";
+import { connect } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
+import { changeText } from "../../store";
 
-export default class Home extends Component {
+class Home extends Component {
     render() {
         return (
             <View>
-                <Text>Home page</Text>
+                <Text style={styles.container}>
+                    Home page, props: {this.props.text}
+                </Text>
+                <Button
+                    onPress={this.props.tick}
+                    title="Learn More"
+                    color="#841584"
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        color: "red",
+        fontSize: 20
+    }
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        tick: () => dispatch(changeText())
+    };
+};
+
+const mapStateToProps = state => {
+    return {
+        text: state.text
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Home);
